7a61d327019ae7e26bba63b17189e551886593e4
Katari <73192062+Katari-Kityama@users.noreply.github.com>
2023-05-03 19:07:49 -0600

    Create UpdateImage.js

---
+++ scripts/UpdateImage.js
@@ -0,0 +1,17 @@
+// Load a new image from the PHP script
+function loadNewImage() {
+  // Make an AJAX request to the PHP script
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://kyvera.000webhostapp.com/update-image.php", true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // If the request was successful, update the image source on the page
+      var img = document.getElementById("my-image");
+      img.src = "https://kyvera.000webhostapp.com/images/selected.png?" + new Date().getTime();
+    }
+  };
+  xhr.send();
+}
+
+// Call the loadNewImage() function when the page is loaded
+window.addEventListener("load", loadNewImage);
